fe1ec26a39d5d812c41fb415e463da353b1ae33f
Anotidaishe <muhambaanotidaishe@gmail.com>
2026-01-14 11:48:54 +0200

    connect to database

--- api/index.js
+++ api/index.js
@@ -4,13 +4,13 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 mongoose
- .connect(process.env.MONGO)
- .then(() => {
-    console.log("Connected to MongoDB");
- })
- .catch((err) => {
-    console.log(err);
- });
+  .connect(process.env.MONGO)
+  .then(() => {
+     console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+     console.log(err);
+  });
 
 const app = express();
 
